bdb84f64d24c2652f1b587770d2405e3d0bac532
Matheus <matvsan@gmail.com>
2020-12-29 21:38:20 -0300

    Movimento por comando

--- public/js/index.js
+++ public/js/index.js
@@ -7,8 +7,8 @@ const key = new Keyboard(document);
 
 var pos = new Vetor2D(0, 0);
 
-const velX = new Vetor2D(1, 0);
-const velY = new Vetor2D(0, 1);
+const velX = new Vetor2D(10, 0);
+const velY = new Vetor2D(0, 10);
 
 const ang = Math.PI / 2;
 const ang2 = Math.PI / 2;
@@ -17,13 +17,28 @@ const rotMatrixNeg = new Matriz2x2(Math.cos(ang2), -Math.sin(ang2), Math.sin(ang
 
 const rotMatrix = rotMatrixPos.multiplicaMatriz(rotMatrixNeg);
 
+let canvas = document.querySelector("#canvas");
+let gl = canvas.getContext("webgl2");
+if (gl) {
+  var galinha = new AssetObject(gl, __dirname + "/../../assets/galinha.png", 75, 100, "galinha", 300, 30, 0, 0, 1, 1);
+  var galo = new AssetObject(gl, __dirname + "/../../assets/Galo.png", 85, 125, "galo", 45, 30, 0, 0, 1, 1);
+}
+
 function main(keyboard) {
   
+  if (gl) {
+    galinha.renderObject();
+    galo.renderObject();
+    
+    galo.updateObjectPosition(pos._x, pos._y);
+    galinha.updateObjectPosition(200, 30);
+  }
+  
   if(keyboard.isKeyPressed('W')){
-    pos = pos.soma(velY);
+    pos = pos.subtrai(velY);
   }
   if(keyboard.isKeyPressed('S')){
-    pos = pos.subtrai(velY);
+    pos = pos.soma(velY);
   }  
   if(keyboard.isKeyPressed('D')){
     pos = pos.soma(velX);
@@ -34,28 +49,14 @@ function main(keyboard) {
   
   let e;    
   while(e = keyboard.peekAndRemoveKeyEvents()){
-
+    
     if(e.type=="keydown" && e.key== "r"){
       pos = rotMatrix.multiplicaVetor(pos);
     }
-
+    
   }
-
+  
   console.log(pos);
 }
 
-let canvas = document.querySelector("#canvas");
-let gl = canvas.getContext("webgl2");
-if (gl) {
-  let galinha = new AssetObject(gl, __dirname + "/../../assets/galinha.png", 75, 100, "galinha", 300, 30, 0, 0, 1, 1);
-  let galo = new AssetObject(gl, __dirname + "/../../assets/Galo.png", 85, 125, "galo", 45, 30, 0, 0, 1, 1);
-  galinha.renderObject();
-  galo.renderObject();
-
-  setTimeout(()=>{
-    galo.updateObjectPosition(100, 30);
-    galinha.updateObjectPosition(200, 30);
-  },5000);
-}
-
-setInterval(function(){ main(key);}, 100);
+setInterval(function(){ main(key);}, 1);
