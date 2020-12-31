1e261c789ca638ce2203ba348c8729db955ca301
Guilherme Teixeira Pimentel <gui-teixeirapimentel@hotmail.com>
2020-12-31 08:57:57 -0300

    Adicionado logs com informacoes de hardware

--- public/js/index.js
+++ public/js/index.js
@@ -15,70 +15,77 @@ const velY = new Vetor2D(0, 1);
 
 const ang = Math.PI / 2;
 const ang2 = Math.PI / 2;
-const rotMatrixPos = new Matriz2x2(Math.cos(ang), -Math.sin(ang), Math.sin(ang), Math.cos(ang)); 
-const rotMatrixNeg = new Matriz2x2(Math.cos(ang2), -Math.sin(ang2), Math.sin(ang2), Math.cos(ang2)); 
+const rotMatrixPos = new Matriz2x2(Math.cos(ang), -Math.sin(ang), Math.sin(ang), Math.cos(ang));
+const rotMatrixNeg = new Matriz2x2(Math.cos(ang2), -Math.sin(ang2), Math.sin(ang2), Math.cos(ang2));
 
 const rotMatrix = rotMatrixPos.multiplicaMatriz(rotMatrixNeg);
 
 persArthur = new Character(new Vetor2D(0, 0), 15, 80);
 
-function main(keyboard) {
-  
-  persArthur.tick();
+// debuging
+console.log(gl.getParameter(gl.RENDERER));
+console.log(gl.getParameter(gl.VENDOR));
+const dbgRenderInfo = gl.getExtension("WEBGL_debug_renderer_info");
+console.log(gl.getParameter(dbgRenderInfo.UNMASKED_VENDOR_WEBGL));
+console.log(gl.getParameter(dbgRenderInfo.UNMASKED_RENDERER_WEBGL));
 
-  let e;    
-  while(e = keyboard.peekAndRemoveKeyEvents()){
-    if(e.type=="keydown"){
-      switch(e.key){
-        case 'w':
-        case 'ArrowUp':
-          persArthur.andarCima();
-          break;
-        case 's':
-        case 'ArrowDown':
-          persArthur.andarBaixo();
-          break;
-        case 'd':
-        case 'ArrowRight':
-          persArthur.andarDireita(); 
-          break;
-        case 'a':
-        case 'ArrowLeft':
-          persArthur.andarEsquerda();
-          break;
-        default:
-      }
+function main(keyboard) {
+
+    persArthur.tick();
+
+    let e;
+    while (e = keyboard.peekAndRemoveKeyEvents()) {
+        if (e.type == "keydown") {
+            switch (e.key) {
+                case 'w':
+                case 'ArrowUp':
+                    persArthur.andarCima();
+                    break;
+                case 's':
+                case 'ArrowDown':
+                    persArthur.andarBaixo();
+                    break;
+                case 'd':
+                case 'ArrowRight':
+                    persArthur.andarDireita();
+                    break;
+                case 'a':
+                case 'ArrowLeft':
+                    persArthur.andarEsquerda();
+                    break;
+                default:
+            }
+        }
+
+        if (e.type == "keyup") {
+            switch (e.key) {
+                case 'w':
+                case 's':
+                case 'd':
+                case 'a':
+                case 'ArrowUp':
+                case 'ArrowDown':
+                case 'ArrowRight':
+                case 'ArrowLeft':
+                    persArthur.parar();
+                    break;
+                default:
+            }
+        }
     }
-    
-    if(e.type=="keyup"){
-      switch(e.key){
-        case 'w':            
-        case 's':
-        case 'd':
-        case 'a':
-        case 'ArrowUp':
-        case 'ArrowDown':
-        case 'ArrowRight':
-        case 'ArrowLeft':
-          persArthur.parar();
-          break;
-        default:
-      }
-    }
-  }
 }
 
 let image = new Image();
 image.src = __dirname + "/../../assets/cyberpunk.png";
 image.onload = () => {
-  let canvas = document.querySelector("#canvas");
-  let gl = canvas.getContext("webgl2");
-  if (!gl) {
-    return;
-  }
+    let canvas = document.querySelector("#canvas");
+    let gl = canvas.getContext("webgl2");
+    if (!gl) {
+        return;
+    }
 
-  shader.renderAsset(gl, image);
-  // window.addEventListener('resize', shader.renderAsset(image));
+    shader.renderAsset(gl, image);
+    // window.addEventListener('resize', shader.renderAsset(image));
 
-  setInterval(function(){ main(key);}, 100);
+    setInterval(function() { main(key); }, 100);
 };
